7cc33e6bce932ba836e064c5af115cf41f304189
Suprit202 <supritmhnt123@gmail.com>
2026-07-12 08:52:33 +0530

    Initial commit

---
+++ TransitOps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransitOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
